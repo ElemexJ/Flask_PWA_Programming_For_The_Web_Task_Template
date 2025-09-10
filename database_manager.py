2cf99a5c7847f0a6b2e4d038752648328c863b79
Joshua <joshua.lumbewe@gmail.com>
2025-09-10 14:19:08 +1000

    asdfg

--- database_manager.py
+++ database_manager.py
@@ -4,6 +4,6 @@ import sqlite3 as sql
 def listExtension():
     con = sql.connect("database/data_source.db")
     cur = con.cursor()
-    data = cur.execute("SELECT * FROM extension").fetchall()
+    data = cur.execute("SELECT * FROM GamePages").fetchall()
     con.close()
     return data
